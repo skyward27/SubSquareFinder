cfffd1353a9db08752b9b4b2b2e4b6eb807c395a
Sean Cruikshank <havesomepie3@hotmail.com>
2020-10-31 18:02:51 -0400

    First two functions made.

---
+++ AlgoProject/dplin.h
@@ -0,0 +1,26 @@
+#ifndef DP_LIN_H
+#define DP_LIN_H
+#include <algorithm>
+void find_area_dp_lin(int* mat, int m, int n, int k, int* startr, int* startc, int* endr, int* endc) {
+	int* buffer = new int[n+1];
+	int max_area, last, int endj, endi;
+	for (int i = 0; i <= m; i++) {
+		for (int j = 0; j <= n; j++) {
+			int temp = buffer[j];
+			if (mat[i - 1][j - 1] >=k) {
+				buffer[j] = std::min(std::min(buffer[j - 1], last), buffer[j]) + 1;
+				if (buffer[j] > max_area) {
+					max_area = buffer[j];
+					endj = j;
+					endi = i;
+				}
+			}
+		}
+	}
+	*startr = endi - max_area - 1;
+	*startc = endj - max_area - 1;
+	*endr = endi;
+	*endc = endj;
+}
+#endif // !DP_LIN_H
+
---
+++ AlgoProject/dppoly.h
@@ -0,0 +1,29 @@
+#ifndef DP_POLY_H
+#define DP_POLY_H
+#include <algorithm>
+void find_area_dp_poly(int *mat, int m, int n, int k,int *startr,int *startc, int *endr, int *endc) {
+	int max_area = 0;
+	int endi, endj;
+	int** buffer = new int* [n];
+	for (int i = 0; i < n, ++i) {
+		buffer[i] = new int[m];
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (mat[i - 1][j - 1] >= k) {
+				buffer[i][j] = std::min(std::min(buffer[i][j - 1], buffer[i - 1][j]), buffer[i - 1][j - 1]) + 1;
+				if (buffer[i][j] > max_area) {
+					endi = i;
+					endj = j;
+					max_area = buffer[i][j];
+				}
+			}
+		}
+	}
+	*startr = endi - max_area - 1;
+	*startc = endj - max_area - 1;
+	*endr = endi;
+	*endc = endj;
+}
+#endif // !DP_POLY_H
+
